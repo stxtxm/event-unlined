7414adae5771f1152ec4e2f0b8530a7a45b2984a
stxtxm <stxtxm@users.noreply.github.com>
2026-02-01 22:48:39 +0000

    deploy: d1b61f49f5e9d914bd021c48a62246e2fc1acbeb

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "event-unlined-v1";
+const urlsToCache = [
+  "/",
+  "/minecraft.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
